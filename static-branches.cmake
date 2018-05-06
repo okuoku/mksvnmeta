027a3f48e7ad7391b4fa266c3199782e8f5bf08f
okuoku <mjt@cltn.org>
2018-05-06 23:04:02 +0900

    Add static-branches.cmake

---
+++ static-branches.cmake
@@ -0,0 +1,13 @@
+set(skiprev 659)
+# FIXME: Don't we need to parse 645-658?
+
+branch_open(/trunk 643 / 643)
+branch_open(/ 0 / 0)
+branch_close(/ 642)
+branch_open(/branches/releases/1.1 644 / 141)
+branch_open(/branches/releases/1.2 644 / 319)
+branch_open(/branches/releases/1.3 644 / 561)
+branch_open(/vendor/jpeglib 644 /trunk/source/Irrlicht/jpeglib 643)
+branch_open(/vendor/libpng 644 /trunk/source/Irrlicht/libpng 643)
+branch_open(/vendor/zlib 644 /trunk/source/Irrlicht/zlib 643)
+branch_open(/branches/SkinnedMesh 658 /trunk 655)
